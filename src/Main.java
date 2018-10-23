e6bb29c67f47d79195e7c92f3f27a1352687d9f9
KimJoonSeo <junjun901225@gmail.com>
2018-10-23 20:59:59 +0900

    github for git pull

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! for local");
         System.out.println("Hello World for git!");
+        System.out.println("Tell my world!");
     }
 }
